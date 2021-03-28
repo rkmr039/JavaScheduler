e4299586724dc311e8a42c38ce1c6a28ea37db9b
Rishab Kumar <rkmr039@gmail.com>
2021-03-28 14:16:45 +0530

    SpringBootScheduler example

---
+++ bootScheduler/src/main/java/bootScheduler/ScheduleTasks.java
@@ -0,0 +1,30 @@
+package bootScheduler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Component
+public class ScheduleTasks {
+	
+	private SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+	
+	// makes Spring run the task on periodic intervals even if the last invocation may still be running
+	@Scheduled(fixedRate = 5000)
+	public void performTask() {
+		System.out.println("FixedRate task performed at " + dateFormat.format(new Date()));
+	}
+	
+	// specifically controls the next execution time when the last execution finishes
+	@Scheduled(initialDelay = 1000, fixedRate = 5000)
+	public void performDelayTask() {
+		System.out.println("Delayed FixedRate task performed at " + dateFormat.format(new Date()));
+	}
+	
+	// run this task at every five minutes
+	@Scheduled(cron = "*/5 * * * * *")
+	public void performCronTask() {
+		System.out.println("Regular task performed using Cron at " + dateFormat.format(new Date()));
+	}
+}
---
+++ bootScheduler/src/main/java/bootScheduler/SpringBootSchedulerMain.java
@@ -0,0 +1,16 @@
+package bootScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringBootSchedulerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulerMain.class, args);
+
+	}
+
+}
